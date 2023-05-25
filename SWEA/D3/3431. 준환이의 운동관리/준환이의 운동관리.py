4c92a33f248b1e65fdf0213729a591be63d57302
Yujun Won <124374862+Yujun-Won@users.noreply.github.com>
2023-05-25 23:55:00 +0900

    [D3] Title: 준환이의 운동관리, Time: 266 ms, Memory: 59,552 KB -BaekjoonHub

---
+++ SWEA/D3/3431. 준환이의 운동관리/준환이의 운동관리.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for i in range(1, T+1):
+    L, U, X = map(int, input().split())
+
+    if L <= X <= U:
+        answer = 0
+    elif X > U:
+        answer = -1
+    else:
+        answer = L - X
+
+    print(f"#{i} {answer}")
