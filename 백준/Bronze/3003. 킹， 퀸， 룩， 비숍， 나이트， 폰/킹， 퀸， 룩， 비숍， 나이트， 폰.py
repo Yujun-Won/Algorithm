50ecaaac2667267c9b9eb227f631ccfc8f4333d3
Yujun Won <124374862+Yujun-Won@users.noreply.github.com>
2023-07-20 22:34:53 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,10 @@
+K, Q, R, B, KN, P = 1, 1, 2, 2, 2, 8
+
+ik, iq, ir, ib, ikn, ip = map(int, input().split())
+
+print(K-ik, end=' ')
+print(Q-iq, end=' ')
+print(R-ir, end=' ')
+print(B-ib, end=' ')
+print(KN-ikn, end=' ')
+print(P-ip)
