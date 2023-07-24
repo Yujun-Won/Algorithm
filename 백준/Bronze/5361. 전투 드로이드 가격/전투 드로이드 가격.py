24fea086e7cf140d0d60b91458403a2c3df1be97
Yujun Won <124374862+Yujun-Won@users.noreply.github.com>
2023-07-24 21:26:08 +0900

    [Bronze III] Title: 전투 드로이드 가격, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/5361. 전투 드로이드 가격/전투 드로이드 가격.py
@@ -0,0 +1,4 @@
+for _ in range(int(input())):
+    A, B, C, D, E = map(int, input().split())
+    result = A * 350.34 + B * 230.90 + C * 190.55 + D * 125.30 + E * 180.90
+    print("${:.2f}".format(result))
