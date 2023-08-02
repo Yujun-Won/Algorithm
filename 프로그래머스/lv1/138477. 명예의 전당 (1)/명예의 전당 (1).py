ce788b3f9f0df4987e851293d00c8e103282eaff
Yujun Won <yujunwon27@naver.com>
2023-08-02 23:55:17 +0900

    chore: unrate -> level 1

--- 프로그래머스/lv1/138477. 명예의 전당 (1)/명예의 전당 (1).py
+++ 프로그래머스/lv1/138477. 명예의 전당 (1)/명예의 전당 (1).py
@@ -1,12 +1,12 @@
 def solution(k, score):
     answer = []
     result = []
-    
+
     for i in score:
         if len(answer) < 3:
             answer.append(i)
             answer = sorted(answer, reverse=True)
-            result.append(answer[-1])        
+            result.append(answer[-1])
         else:
             temp = answer.pop()
             if i < temp:
@@ -16,5 +16,5 @@ def solution(k, score):
                 answer.append(i)
                 answer = sorted(answer, reverse=True)
                 result.append(answer[-1])
-            
+
     return result
