7ded0e09bff05f8b54ac8cb85cc75742fc8970e9
Yujun Won <124374862+Yujun-Won@users.noreply.github.com>
2023-08-28 22:06:05 +0900

    [Silver II] Title: 최대 힙, Time: 128 ms, Memory: 37180 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,14 @@
+import sys, heapq
+
+heap = []
+
+for _ in range(int(sys.stdin.readline())):
+    num = int(sys.stdin.readline())
+
+    if num == 0:
+        if len(heap) > 0:
+            print(-heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, -num)
